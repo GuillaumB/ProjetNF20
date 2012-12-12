1d6c055cadf8bfcf96badf0de8c91e1d4e5d7625
GuillaumB <bourbon.guillaume@gmail.com>
2012-12-12 15:42:50 +0100

    Work In Progress

--- README.txt
+++ README.txt
@@ -1,5 +1,7 @@
 ProjetNF20
-==========
+============
 
 Implémentation des algorithmes de Prim et Kruskal en Python. 
+
+
 
--- src/main.py
+++ src/main.py
@@ -21,10 +21,6 @@ def Main():
   # Appel du parser de fichier
   infoGraph = Parser(args.data)
 
-  newGraph = Prim(infoGraph)
-
-  print(newGraph)
-
   result = Prim(infoGraph)
  
   print("result "+str(result)) 
@@ -55,6 +51,7 @@ def Parser(data):
   for ligne in fichier:
     if nbrLigne==0: # On extrait le type de graph
       if ligne.lower().find("graph")>=0:
+        ligne  = ligne.rstrip()
         typeGraph = ligne.rstrip('\n\r')
       else:
         Error("Erreur de Formatage : Type de Graphe")
@@ -62,14 +59,14 @@ def Parser(data):
     elif nbrLigne==1: # On extrait le nombre de sommet
       if ligne.lower().find("nb_nodes")>=0:
         nbrNode = ligne.rstrip('\n\r')
-        nbrNode = nbrNode.split("\t")[2].replace(" ","")
+        nbrNode = nbrNode.replace("\t", "").split()[1]
       else:
         Error("Erreur de Formatage : Nombre de Sommets")
 
     elif nbrLigne == 2: # On extrait le nombre d'arcs
       if ligne.lower().find("nb_edges")>=0:
         nbrEdge = ligne.rstrip('\n\r')
-        nbrEdge = nbrEdge.split("\t")[2].replace(" ","")
+        nbrEdge = nbrEdge.replace("\t", "").split()[1]
       else:
         Error("Erreur de Formatage : Nombre d'Arcs")
 
@@ -77,8 +74,8 @@ def Parser(data):
       if ligne.rstrip('\n\r') == "END": # Fin des données, on sort de la boucle
         break
       else:
-        temp = ligne.rstrip('\n\r').replace(" ","") # On dégage les espaces en trop
-        temp = temp.split("\t") # On sépare les données grâce aux tabulation du fichier
+        temp = ligne.replace("\t", " ").rstrip('\n\r') # On remplace les tabulations pas de espaces
+        temp = temp.split() # On découpe le tout en trois grâce aux espaces
         for i, value in enumerate(temp): # Permet de convertir les données
           if i != 0 and i%2 == 0:
             try:
@@ -95,11 +92,22 @@ def Parser(data):
   fichier.close() # Fermeture du fichier
 
 
-  return {'Title': typeGraph, 'Nodes': nbrNode, 'Edges': nbrEdge, 'Arcs': listArcs}# On retourne un dictionnaire avec l'ensemble des info parser !
+  return {'Type': typeGraph, 'Nodes': nbrNode, 'Edges': nbrEdge, 'Arcs': listArcs}# On retourne un dictionnaire avec l'ensemble des info parser !
 
 
 def Prim(graph):
-  """Algorithme de Prim
+  if graph['Type'].upper() == "UNDIRECTED GRAPH":
+    return PrimUndirected(graph)
+
+  elif graph['Type'].upper() == "DIRECTED GRAPH":
+    return PrimDirected(graph)
+
+  else:
+    print("Type de Graphe inconnu !")
+    quit()
+
+def PrimUndirected(graph):
+  """Algorithme de Prim pour les graphes non-orientés
   Retourne la liste des sommets du chemin de cout minimum et la liste des arcs qui composent le nouveau graphe 
   """
 
@@ -152,6 +160,57 @@ def Prim(graph):
     arcToChoose = None
     edgeToChoose = None
 
+
+  return {'Edges':usedEdges, 'Arcs': usedArcs}
+
+def PrimDirected(graph):
+  """Algorithme de Prim pour les graphes orientés
+  Retourne la liste des sommets du chemin de cout minimum et la liste des arcs qui composent le nouveau graphe 
+  """
+
+  nbrNode = graph['Nodes']
+  listArcs = graph['Arcs']
+  usedEdges = []
+  usedArcs = []
+
+  tempArcs = []
+  
+  arcToChoose = None
+  edgeToChoose = None
+
+  usedEdges.append(listArcs[0][0]) # On choisi un sommet pour on commence le graphe
+
+  while len(usedEdges) < int(nbrNode): # tant qu'on est pas passé par tous les sommets
+    actualEdge = usedEdges[-1] # On choisi le dernier somment sur lequel on est arrivé
+
+    #print("actualEdges "+actualEdge)
+
+    for i in listArcs:
+      if actualEdge in i:
+        tempArcs.append(i) # on liste l'ensemble des arcs sortant du point actuel et qui ne va pas vers point déjà utilisé
+
+
+    for j in usedArcs: # on enlève des arcs possible les arcs sur lesquels on est déjà passés
+      if j in tempArcs:
+        tempArcs.remove(j)
+
+    #print(tempArcs)
+
+    tempMinWeight = tempArcs[0][2] # On prend une valeur de poids parmis les arcs possible
+
+    for i, value in enumerate(tempArcs):
+      if value[2] <= tempMinWeight: # Si le poids de l'arete teste est inférieur ou egale au poids référence alors on mémorise le poid l'arete et le futur sommet
+        tempMinWeight = value[2] 
+        arcToChoose = value
+        edgeToChoose = value[1]
+        
+    usedEdges.append(edgeToChoose)
+    usedArcs.append(arcToChoose)
+    
+    # Reset
+    tempArcs = []
+    arcToChoose = None
+    edgeToChoose = None
 
   return {'Edges':usedEdges, 'Arcs': usedArcs}
 
