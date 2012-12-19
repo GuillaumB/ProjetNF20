48d75d57394744f037cde14b8af151987bf150db
Guillaum B. <bourbon.guillaume@gmail.com>
2012-12-19 16:01:39 +0100

    Work In Progress
    Prim + Kruskal > OK
    UnionFind sur les deux à confirmer

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@
 import os
 import argparse
 
-from disjointset import DisjointSet 
+from DisjointSet import DisjointSet 
 
 from operator import itemgetter
 
@@ -13,7 +13,7 @@ from operator import itemgetter
 def Main():
   '''Fonction Principal
   '''
-  print("----- Projet NF20 -----".center(60,"-")+"\n")
+  print("\n"+"----- Projet NF20 -----".center(70,"-")+"\n")
 
   # Création du parser d'aguments
   parser = argparse.ArgumentParser(description="Script Arguments Parser")
@@ -27,11 +27,17 @@ def Main():
   graph = Parser(args.data)
 
   if args.activePrim:
-    print("Prim : "+str(Prim(graph)))
+    resultPrim = Prim(graph)
+    poidsPrim = CompterPoids(resultPrim)
+    print("Prim : "+str(resultPrim))
+    print("pour un poids de "+str(poidsPrim))
 
   if args.activeKruskal:
-    print("Kruskal : "+str(Kruskal(graph)))
-  
+    resultKruskal = Kruskal(graph)
+    poidsKruskal = CompterPoids(resultKruskal)
+    print("Kruskal : "+str(resultKruskal))
+    print("pour un poids de "+str(poidsKruskal))
+
   if not args.activePrim and not args.activeKruskal:
     print("Resultat du Parser : "+str(graph))
 
@@ -44,7 +50,6 @@ def Error(msg):
   """
   print(msg)
   quit()
-
 
 def Parser(data):
   """Fonction permettant de parser le fichier .dat  
@@ -67,14 +72,14 @@ def Parser(data):
     elif nbrLigne==1: # On extrait le nombre de sommet
       if ligne.lower().find("nb_nodes")>=0:
         nbrNode = ligne.rstrip('\n\r')
-        nbrNode = nbrNode.replace("\t", "").split()[1]
+        nbrNode = nbrNode.replace("\t", " ").split()[1]
       else:
         Error("Erreur de Formatage : Nombre de Sommets")
 
     elif nbrLigne == 2: # On extrait le nombre d'arcs
       if ligne.lower().find("nb_edges")>=0:
         nbrEdge = ligne.rstrip('\n\r')
-        nbrEdge = nbrEdge.replace("\t", "").split()[1]
+        nbrEdge = nbrEdge.replace("\t", " ").split()[1]
       else:
         Error("Erreur de Formatage : Nombre d'Arcs")
 
@@ -105,8 +110,15 @@ def Parser(data):
 
   return {'Type': typeGraph, 'Nodes': nodes, 'Edges': edges}# On retourne un dictionnaire avec l'ensemble des info parser !
 
+def CompterPoids(liste):
+  poids = 0;
 
-def Prim(graph):
+  for edge in liste:
+    poids += edge[2]
+
+  return poids
+
+def PrimOld(graph):
   """Algorithme de Prim
   Retourne la liste des sommets du chemin de cout minimum et la liste des arcs qui composent le nouveau graphe 
   """
@@ -126,18 +138,13 @@ def Prim(graph):
   while len(usedEdges) < int(nbrNode): # tant qu'on est pas passé par tous les sommets
     actualEdge = usedEdges[-1] # On choisi le dernier somment sur lequel on est arrivé
 
-    #print("actualEdges "+actualEdge)
-
     for i in listArcs:
       if actualEdge in i:
         tempArcs.append(i) # on liste l'ensemble des arcs sortant du point actuel et qui ne va pas vers point déjà utilisé
 
-
     for j in usedArcs: # on enlève des arcs possible les arcs sur lesquels on est déjà passés
       if j in tempArcs:
         tempArcs.remove(j)
-
-    #print(tempArcs)
 
     tempMinWeight = tempArcs[0][2] # On prend une valeur de poids parmis les arcs possible
 
@@ -166,22 +173,70 @@ def Prim(graph):
     arcToChoose = None
     edgeToChoose = None
 
+  # return {'Edges': usedEdges, 'Arcs': usedArcs}
+  return usedArcs
 
-  return {'Edges': usedEdges, 'Arcs': usedArcs}
-
-def Kruskal(graph):
-  """Algorithme de Kruskal
-  Retourne la liste des sommets du chemin de cout minimum
+def Prim(graph):
+  """Algorithme de Prim + Union-Find
+  Retourne la liste des arretes du graphe de coût minimum
   """
 
   forest = DisjointSet()
-  mst = []
+  chemin = []
+  tempEdges = []
+  usedNodes = [] 
 
   for n in graph['Nodes']:
     forest.add(n)
 
-  size = len(graph['Nodes']) - 1
+  nbrNodes = len(graph['Nodes'])
 
+  origin = graph['Nodes'][0]
+
+  usedNodes.append(origin)
+
+  while(len(chemin) != nbrNodes-1):
+    tempNodes = []
+    tempEdges = []    
+
+    for node in forest:
+      if forest[node] in usedNodes: # On cherche tous les sommets dont le père fait parti des sommet déjà parcouru
+        tempNodes.append(node)
+
+    for node in tempNodes:
+      for edge in graph['Edges']:
+        if node in edge and edge not in chemin:
+          tempEdges.append(edge)
+
+    tempEdges = sorted(tempEdges, key=itemgetter(2))   
+
+    chemin.append(tempEdges[0]) # On prend l'arrete avec le plus petit poids
+
+    if graph['Type'].upper() == "UNDIRECTED GRAPH":
+      if chemin[-1][0] in usedNodes:
+        forest.union(chemin[-1][0], chemin[-1][1])
+        usedNodes.append(chemin[-1][0])
+      else:
+        forest.union(chemin[-1][1], chemin[-1][0])
+        usedNodes.append(chemin[-1][1])             
+    else:
+      forest.union(chemin[-1][1], chemin[-1][0])
+      usedNodes.append(chemin[-1][1])
+
+  return chemin
+
+def Kruskal(graph):
+  """Algorithme de Kruskal
+  Retourne la liste des arretes du graphe de coût minimum
+  """
+
+  forest = DisjointSet()
+  chemin = []
+
+  for n in graph['Nodes']:
+    forest.add(n)
+
+  nbrNodes = len(graph['Nodes']) - 1
 
   for arc in sorted(graph['Edges'], key=itemgetter(2)):
     n1, n2, poids = arc
@@ -190,11 +245,11 @@ def Kruskal(graph):
     t2 = forest.find(n2)
 
     if t2 != t1:
-      mst.append(arc)
-      size -= 1
+      chemin.append(arc)
+      nbrNodes -= 1
 
-      if size == 0:
-        return mst
+      if nbrNodes == 0:
+        return chemin
 
       forest.union(t1, t2)
 
